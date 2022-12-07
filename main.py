c7e6f9da03630c4e21d3e760361d4e8d75fbe128
furkantahabademci <tahafurkanbademci@gmail.com>
2022-12-07 18:10:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob 
 
+paths = glob.glob("./images/*/*.jpg")
+
+for path in paths:
+    
+    data_augmenter
+
+
+
+def data_augmenter():
